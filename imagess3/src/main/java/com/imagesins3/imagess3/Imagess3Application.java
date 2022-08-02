db2edbe69510bab3d8f27d64224b0b86797912f2
Erika Brito <erikajohanabrito@gmail.com>
2022-08-02 14:26:44 -0300

    Init commit

---
+++ imagess3/src/main/java/com/imagesins3/imagess3/Imagess3Application.java
@@ -0,0 +1,13 @@
+package com.imagesins3.imagess3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imagess3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imagess3Application.class, args);
+	}
+
+}
---
+++ imagess3/src/test/java/com/imagesins3/imagess3/Imagess3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagesins3.imagess3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imagess3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
